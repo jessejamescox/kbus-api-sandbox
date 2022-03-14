fef944f51067359c57cbac65ade6a695f32aaee8
Jesse Cox <jess.coxpdx@gmail.com>
2022-03-14 14:47:05 -0700

    updated mosquitto src

--- mqtt.h
+++ mqtt.h
@@ -5,5 +5,9 @@
 #include "kbus-api.h"
 
 extern void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message);
+
 extern void connect_callback(struct mosquitto *mosq, void *obj, int result);
+
+extern void disconnect_callback(struct mosquitto *mosq, void *obj, int result);
+
 #endif /*__MQTT_H__*/
